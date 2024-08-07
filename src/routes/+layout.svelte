7e08f6bc1e331195848d47d572623c08f02ee495
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-08-07 22:01:21 +0300

    footer: added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import Header from '$lib/components/Header/Header.svelte';
+	import Footer from '$lib/components/Footer/Footer.svelte';
 	let { children } = $props();
 </script>
 
@@ -8,3 +9,4 @@
 <main>
 	{@render children()}
 </main>
+<Footer />
